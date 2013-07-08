29551717003287aeddd6325bd1f35877c6901cc7
Panisuan Chasinga <jo.chasinga@gmail.com>
2013-07-08 18:30:38 +0000

    Change web.js again

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
+var sentence = fs.readFileSync("index.html","utf-8");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(sentence);
 });
 
 var port = process.env.PORT || 5000;
