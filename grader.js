d2e79544f69addf339e6fdb29686feadd1d607f5
Panisuan Chasinga <jo.chasinga@gmail.com>
2013-07-15 10:10:47 +0000

    Styled index.html and configure PORT in web.js to 8080

--- grader.js
+++ grader.js
@@ -1,32 +1,15 @@
-#!/Usr/Bin/-env node
-/*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
+#!/usr/bin/env node
 
 var fs = require('fs');
+var sys = require('util');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://calm-cliffs-1788.herokuapp.com"
 
+// Test that file exists, and return the file name type 'string'
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -49,7 +32,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+        var present = $(checks[ii]).length > 0; // check checksfile against htmlfile
         out[checks[ii]] = present;
     }
     return out;
@@ -65,10 +48,26 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'User-defined URL', String, URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url != null) {
+        htmlfile = "file.html";
+        rest.get(program.url).on('complete', function(result) {
+            if(result instanceof Error) {
+                console.error('error: ' + result.message);
+            } else {
+                fs.writeFileSync(htmlfile, result);
+                var checkJson = checkHtmlFile(htmlfile, program.checks);
+                var outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);  
+            }
+        });
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
